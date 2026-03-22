b32d474b6a28e9f1c29e83486cf9157f3ac2feae
RtkMor <rtkmor@gmail.com>
2026-03-22 18:07:52 +0530

    testing push

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ const TABS = ['menu', 'chat', 'cart', 'admin'];
 function App() {
   const [activeTab, setActiveTab] = useState('menu'); // 'menu', 'chat', 'cart', 'admin'
   const [menu, setMenu] = useState([]);
-  //
   const [orders, setOrders] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState('All');
   const [cart, setCart] = useState([]);
