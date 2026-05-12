8be24e1ab01703c8d72b19989ad6e1eed0133c55
Rahul <rahul.m@fnp.com>
2026-05-12 17:18:20 +0530

    login page bg color

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -54,7 +54,7 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="min-h-screen bg-slate-950 flex items-center justify-center px-4">
+    <div className="min-h-screen  flex items-center justify-center px-4">
       <div className="w-full max-w-sm">
         {/* Back to home */}
         <div className="text-center mb-4">
